b0f05e759d750ea064204e3b34d1cfab294e4f68
James Elford <james.elford08@imperial.ac.uk>
2011-12-25 01:42:50 +0000

    Added a second class. This class contains a method with void signature. Currently, Please will not deal with this special case.

---
+++ src/src/ASecondClass.java
@@ -0,0 +1,12 @@
+package src;
+
+import gen.Expose;
+
+import java.io.PrintStream;
+
+public class ASecondClass {
+
+	private void someFunction(ASecondClass e, PrintStream out) {
+		out.println("Hello, world");
+	}
+}
