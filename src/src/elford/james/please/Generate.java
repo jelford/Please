b694cbf0696bc321dd4148361e3b83563e62aac5
James Elford <james.elford08@imperial.ac.uk>
2011-12-26 16:40:11 +0000

    Moved a bunch of string literals out into methods to make maintenence easier

--- src/src/elford/james/please/Generate.java
+++ src/src/elford/james/please/Generate.java
@@ -1,6 +1,7 @@
 package src.elford.james.please;
 
 import java.io.IOException;
+import java.io.PrintStream;
 
 import javassist.CannotCompileException;
 import javassist.ClassPool;
@@ -23,35 +24,51 @@ public class Generate {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		ClassPool cp = ClassPool.getDefault();
 		CtClass please = cp.makeClass("gen.elford.james.please.Please");
 
 		Discoverer disco = new ClasspathDiscoverer();
-		disco.addAnnotationListener(new ExposeAnnotationDiscoveryListener(cp, please));
+		disco.addAnnotationListener(new ExposeAnnotationDiscoveryListener(cp, please, System.out));
 		disco.discover();
 		
-		try {
-			please.writeFile("gen");
-		} catch (CannotCompileException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		please.writeFile("gen");
 
 	}
 
 	static class ExposeAnnotationDiscoveryListener implements
 			ClassAnnotationDiscoveryListener {
 
-		private ClassPool cp;
-		private CtClass please;
+		private final ClassPool cp;
+		private final CtClass please;
+		private final PrintStream out;
 		
-		public ExposeAnnotationDiscoveryListener(ClassPool cp, CtClass please) {
+		private final String startFunctionBody = "{";
+		private final String startTryBlock = "try {";
+		
+		
+		private final String endTryBlock = 
+			"} catch (SecurityException e) {" +
+				"e.printStackTrace();" +
+			"} catch (NoSuchMethodException e) {" +
+				"e.printStackTrace();" +
+			"} catch (IllegalArgumentException e) {" +
+				"e.printStackTrace();" +
+			"} catch (IllegalAccessException e) {" +
+				"e.printStackTrace();" +
+			"} catch (java.lang.reflect.InvocationTargetException e) {" +
+				"e.printStackTrace();" +
+			"}";
+		
+		private final String endFunctionBody = "}";
+		private final String setMethodAccessible = "m.setAccessible(true);";
+		private final String methodName = "m";
+		
+		
+		public ExposeAnnotationDiscoveryListener(ClassPool cp, CtClass please, PrintStream out) {
 			this.cp = cp;
 			this.please = please;
+			this.out = out;
 		}
 		
 		@Override
@@ -62,7 +79,7 @@ public class Generate {
 		@Override
 		public void discovered(String clazz, String annotation) {
 			assert Expose.class.getName().equals(annotation);
-			System.out.println(clazz);
+			out.println(clazz);
 
 			try {
 
@@ -85,8 +102,7 @@ public class Generate {
 				CtMethod[] ctmMethods = ctm.getDeclaredMethods();
 				for (CtMethod method : ctmMethods) {
 					String name = method.getName();
-					boolean hasReturn = !method.getReturnType().getName().equals("void");
-					boolean isPrimitive = method.getReturnType().isPrimitive();
+					boolean hasReturn = !method.getReturnType().equals(CtClass.voidType);
 					
 					MethodInfo info = method.getMethodInfo();
 					int accessFlags = info.getAccessFlags();
@@ -100,62 +116,35 @@ public class Generate {
 						
 						
 						StringBuffer mBody = new StringBuffer();
-						mBody.append(
-						"{" +
-							"try {"+
-								// Get a hold on the method
-								"java.lang.reflect.Method m = wrapped.getClass().getDeclaredMethod(\""+method.getName()+"\", ");
-										if (method.getParameterTypes().length > 0) {
-											mBody.append("new Class[] {");
-											for(CtClass paramType : method.getParameterTypes()) {
-												mBody.append(paramType.getName() + ".class");
-												mBody.append(",");
-											}
-											mBody.replace(mBody.length()-1, mBody.length(), "");
-											mBody.append("}");
-										} else {
-											mBody.append("null");
-										}
-								mBody.append(
-										");");
-								
-								// Expose the method
-								mBody.append("m.setAccessible(true);"); 
-								
-								// Cast for return
-								if (hasReturn) {
-									mBody.append("return ("+method.getReturnType().getName()+") ");
-								}
-								
-								// Invoke the method
-								mBody.append("m.invoke(wrapped, new Object[] {");
-								for (int i=0; i < method.getParameterTypes().length; ++i) {
-									mBody.append("$"+(Integer.toString(i+1))+", ");
-								}
-								mBody.replace(mBody.length()-2, mBody.length(), "");
-			
-								mBody.append("});");
-								
-								
-							// Exception handling
-							mBody.append(	
-							"} catch (SecurityException e) {" +
-								"e.printStackTrace();" +
-							"} catch (NoSuchMethodException e) {" +
-								"e.printStackTrace();" +
-							"} catch (IllegalArgumentException e) {" +
-								"e.printStackTrace();" +
-							"} catch (IllegalAccessException e) {" +
-								"e.printStackTrace();" +
-							"} catch (java.lang.reflect.InvocationTargetException e) {" +
-								"e.printStackTrace();" +
-							"}" + 
-							(hasReturn ? "return null;" : "") +
-							
-						"}");
 						
-						System.out.println(mBody);
+						mBody.append(startFunctionBody);
+						mBody.append(startTryBlock);
+						
+						// Method m = ...
+						mBody.append(introspectMethodWithSignature(method));
+
+						// Expose the method
+						mBody.append(setMethodAccessible); 
+								
+						// Return statement, with appropriate casting
+						if (hasReturn) {
+							mBody.append(returnStatement(method));
+						}
+						
+						// Invoke the method
+						mBody.append(invokeMethod(method));
+								
+								
+						// Close the try block and deal with exceptions
+						mBody.append(endTryBlock);
+						
+						// A return type (for if there is an exception)
+						mBody.append(returnGuard(hasReturn));
 							
+						mBody.append(endFunctionBody);
+						
+						out.println(mBody);
+
 							
 						CtMethod wrappedPrivateMethod = CtNewMethod.make(
 								method.getReturnType(), name,
@@ -189,6 +178,56 @@ public class Generate {
 				e.printStackTrace();
 			}
 		}
+		
+		private String returnGuard(boolean hasReturn) {
+			return (hasReturn ? "return null;" : "");
+		}
+
+		private String invokeMethod(CtMethod method) throws NotFoundException {
+			StringBuilder invocation = new StringBuilder();
+			invocation.append(methodName + ".invoke(wrapped, new Object[] {");
+			for (int i=0; i < method.getParameterTypes().length; ++i) {
+				invocation.append("$"+(Integer.toString(i+1))+", ");
+			}
+			invocation.replace(invocation.length()-2, invocation.length(), "");
+	
+			invocation.append("});");
+			
+			
+			return invocation.toString();
+		}
+
+		private String returnStatement(CtMethod method) throws NotFoundException {
+			//boolean isPrimitive = method.getReturnType().isPrimitive();
+			
+			return "return ("+method.getReturnType().getName()+") ";
+		}
+
+		private String introspectMethodWithSignature(CtMethod method) throws NotFoundException {
+			StringBuilder introspectMethod = new StringBuilder();
+			introspectMethod.append(
+					"java.lang.reflect.Method " + methodName + " = " + 
+						"wrapped.getClass().getDeclaredMethod(\""+method.getName()+"\", ");
+			
+			
+			if (method.getParameterTypes().length > 0) {
+				introspectMethod.append("new Class[] {");
+				for(CtClass paramType : method.getParameterTypes()) {
+					introspectMethod.append(paramType.getName() + ".class");
+					introspectMethod.append(",");
+				}
+				introspectMethod.replace(introspectMethod.length()-1, introspectMethod.length(), "");
+				introspectMethod.append("}");
+			} else {
+				introspectMethod.append("null");
+			}
+			introspectMethod.append(");");
+			
+			
+				
+			return introspectMethod.toString();
+		}
+		
 
 	}
 }
